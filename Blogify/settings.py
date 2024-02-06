79d315c438ee8c7909ff45290f4f775bdace5546
Nikhil <nikhilmore5032@gmail.com>
2024-02-06 19:20:37 +0530

    static files served

--- Blogify/settings.py
+++ Blogify/settings.py
@@ -14,6 +14,12 @@ from pathlib import Path
 import dj_database_url
 import os
 
+#for getting environment variables
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'env', '.env')
+load_dotenv(dotenv_path)
+#environment variable end
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,6 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
